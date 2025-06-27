404a600ad5c5c184d7634ccd67d251b497db8442
ThotaSridharRao <t.shridher@gmail.com>
2025-06-28 02:43:17 +0530

    made changes

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -1,14 +1,14 @@
 import ffmpeg
-import os
+import subprocess
 
 async def convert_video_format(input_file_path: str, output_file_path: str):
     try:
-        # Use FFmpeg to convert video format
         (
             ffmpeg
             .input(input_file_path)
             .output(output_file_path)
-            .run(overwrite_output=True)
+            .run(overwrite_output=True, capture_stdout=True, capture_stderr=True)
         )
     except ffmpeg.Error as e:
+        print("🚨 FFmpeg stderr:", e.stderr.decode())
         raise Exception("FFmpeg conversion failed") from e
